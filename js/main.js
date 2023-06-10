effb98f8ab7ec266a12ccd4fc96f29578a9c776c
Janet <janet.mamani@itscordoba.edu.ar>
2023-06-09 21:19:56 -0300

    se modificaron todos los archivos

--- js/main.js
+++ js/main.js
@@ -5,15 +5,57 @@ botones.forEach(boton => {
     boton.addEventListener("click",() => {
         const botonapretado = boton.textContent;
         
-        
-        if (pantalla.textContent =="|") {
+        if (pantalla.textContent === "|") {
             pantalla.textContent = botonapretado;
         }else {
             pantalla.textContent += botonapretado;
-            return;
           }
-        
-    
-    
+        if (botonapretado === "+"){
+            resultado = (num1 + num2);
+        }
+        if (botonapretado === "=") {
+            botonapretado = resultado;
+            pantalla.textContent(resultado);
+        }
+        if (botonapretado === "C"){
+            pantalla.textContent = "|"
+        }
+
     })
 })
+
+/*switch (botonapretado)  {
+    case "+":
+        operacion="suma"
+        num1 = botonapretado;
+        num2 = botonapretado;
+        resultado  = ( num1 + num2);
+        pantalla.textContent = resultado
+    case "-":
+        operacion = "resta";
+        num1 = botonapretado;
+        num2 = botonapretado;
+        resultado  = ( num1 - num2);
+        pantalla.textContent = resultado
+        
+    case "/":
+        operacion = "division";
+        num1 = botonapretado;
+        num2 = botonapretado;
+        resultado  = ( num1 / num2);
+        pantalla.textContent = resultado
+    case "*":
+        operacion = "multiplicacion";
+        num1 = botonapretado;
+        num2 = botonapretado;
+        resultado  = ( num1 * num2);
+        resultado = pantalla.textContent;
+        break;
+    case "=":
+        operacion = "igual";
+        "igual.button" = resultado;
+        
+    default:
+        operacion = "operacion no valida"; 
+}
+*/
